336160df132f684b300724c72b882f15d4ce8656
Adrien Jayat <adrien.jayat@gmail.com>
2021-02-06 04:50:37 +0100

    fix: add dot-env and load it

--- DjangoOfficialTuto/settings.py
+++ DjangoOfficialTuto/settings.py
@@ -14,11 +14,12 @@ import os
 import mimetypes
 
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
